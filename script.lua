a3318ab704e20bfdc7e266d02220ec6c638a3623
Kaiddd <37759450+Kaiddd@users.noreply.github.com>
2022-09-28 05:10:09 +0000

    im so smart

--- script.lua
+++ script.lua
@@ -8,17 +8,19 @@ local gemLimit = 400000 -- Gem count the farm will stop at, avoids you getting t
 local mode = "unsafe" -- Modes : "safe" ; "unsafe" ; "veryunsafe"
 --End Settings
 
-local plrs = game:GetService("Players")
-local plr = plrs.LocalPlayer
-
-repeat task.wait() until game:IsLoaded() and plr.Character;plr.Character:WaitForChild("Humanoid");task.wait(1)
+repeat task.wait() until game:IsLoaded()
 
 math.randomseed(tick())
+local plrs = game:GetService("Players")
+local plr = plrs.LocalPlayer
 local repStorage = game:GetService("ReplicatedStorage")
 local ls = game:GetService("LogService")
 local ws = game:GetService("Workspace")
 local tws = game:GetService("TweenService")
 local https = game:GetService("HttpService")
+
+repeat task.wait() until plr.Character and plr.Character:WaitForChild("Humanoid")
+task.wait(1)
 
 local UI = game:GetObjects("rbxassetid://11103122108")[1]
 
